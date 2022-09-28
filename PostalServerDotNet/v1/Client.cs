d978d1afd3bf78bb320e0f659f26c8f99fb8d7f1
Guy Maness <guy.maness@gmail.com>
2022-09-28 14:32:05 -0400

    Apply suggestions from code review
    Co-authored-by: Nate H <nateh777@users.noreply.github.com>

--- PostalServerDotNet/v1/Client.cs
+++ PostalServerDotNet/v1/Client.cs
@@ -1,4 +1,28 @@
-﻿using Newtonsoft.Json;
+// <copyright>
+// MIT License
+// 
+// Copyright (c) 2022 Kingdom First Solutions
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+// </copyright>
+
+using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 using PostalServerDotNet.v1.Model.Response;
 using RestSharp;
@@ -46,7 +70,13 @@ namespace PostalServerDotNet.v1
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
             var response = _client.Execute<T>( request );
 
-            if ( response.Content.Contains( "error" ) )
+            if ( response.ErrorException != null )
+            {
+                const string message = "Error retrieving response.  Check inner details for more info.";
+                var exception = new ApplicationException( message, response.ErrorException );
+                throw exception;
+            }
+            else if ( response.Content.Contains( "error" ) )
             {
                 var errorResponse = JsonConvert.DeserializeObject<ErrorResponse>( response.Content );
                 var message = string.Format( "Error Code \"{0}\". {1}", errorResponse.Data.Code, errorResponse.Data.Message );
